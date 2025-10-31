80f689b021bf0e78017905e1a659f3d620a71243
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-10-31 14:00:16 +0100

    build: build and publish snapshot packages (#17)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     id("java-library")
     id("com.diffplug.spotless") version "7.2.1"
+    id("maven-publish")
 }
 
 group = "dev.pcvolkmer.onco"
@@ -71,5 +72,36 @@ spotless {
         importOrder()
         removeUnusedImports()
         googleJavaFormat()
+    }
+}
+
+publishing {
+    repositories {
+        mavenLocal()
+        maven {
+            name = "GitDnpmDev"
+
+            val releasesRepoUrl = uri("https://git.dnpm.dev/api/packages/public/maven")
+            val snapshotsRepoUrl = uri("https://git.dnpm.dev/api/packages/public-snapshots/maven")
+            url = if (version.toString().endsWith("SNAPSHOT"))
+                snapshotsRepoUrl
+            else
+                releasesRepoUrl
+
+            credentials(HttpHeaderCredentials::class) {
+                name = "Authorization"
+                value = "token ${properties["dnpm_dev_token"] ?: ""}"
+            }
+
+            authentication {
+                create<HttpHeaderAuthentication>("header")
+            }
+        }
+    }
+
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
     }
 }
