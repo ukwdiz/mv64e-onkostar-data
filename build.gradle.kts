9a63d6798a01f9afe2e350e0031040d207f02389
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-12-15 17:03:36 +0100

    deps!: update dto lib to version 0.2.0 (#83)
    This release is for use with DNPM:DIP (api-gateway) Version >= 1.2.3 and uses patients birthDay and date of death in format 'yyyy-MM' (without day) with fallback to accept old format 'yyyy-MM-dd'.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ group = "dev.pcvolkmer.mv64e"
 version = "0.2.5" // x-release-please-version
 
 var versions = mapOf(
-    "mtb-dto" to "0.1.2",
+    "mtb-dto" to "0.2.0",
     "commons-csv" to "1.10.0",
     "slf4j" to "2.0.17",
     "junit" to "5.14.1",
